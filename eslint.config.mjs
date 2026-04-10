762e0d1beea9165dc1a39bd974a3029ceb711628
temetvince <temetvince@gmail.com>
2026-04-10 09:50:47 -0500

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
+    {
+        ignores: ['node_modules/**', 'dist/**'],
+    },
     tseslint.configs.strictTypeChecked,
     {
         languageOptions: {
